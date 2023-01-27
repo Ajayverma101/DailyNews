9d77bdccbd7c1e0d617d622dcc544b673b878d9c
Ajayverma101 <ajayverma11011999@gmail.com>
2023-01-27 14:29:06 +0530

    add uselocation hook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,15 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../App.css";
+import { useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation()
+  
+  useEffect(()=>{
+
+  },[location])
+
   return (
     <div className="this-is-header fixed-top">
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
@@ -26,37 +33,37 @@ const Header = () => {
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <Link className="nav-link " aria-current="page" to="/">
+                <Link className={`nav-link ${location.pathname=== "/"? "active" : ""}`} aria-current="page" to="/">
                   Home
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/business">
+                <Link className={`nav-link ${location.pathname=== "/business"? "active" : ""}`} to="/business">
                   Business
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/entertainment">
+                <Link className={`nav-link ${location.pathname=== "/entertainment"? "active" : ""}`} to="/entertainment">
                   Entertainment
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/health">
+                <Link className={`nav-link ${location.pathname=== "/health"? "active" : ""}`} to="/health">
                   Health
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/science">
+                <Link className={`nav-link ${location.pathname=== "/science"? "active" : ""}`} to="/science">
                   Science
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/sports">
+                <Link className={`nav-link ${location.pathname=== "/sports"? "active" : ""}`} to="/sports">
                   Sports
                 </Link>
               </li>
               <li className="nav-item">
-                <Link className="nav-link" to="/technology">
+                <Link className={`nav-link ${location.pathname=== "/technology"? "active" : ""}`} to="/technology">
                   Technology
                 </Link>
               </li>
